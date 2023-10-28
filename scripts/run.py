ee31af4c98d7024e3b63a484fc122acca974b60d
thinkabout4x <pave-shherbakov@yandex.ru>
2023-10-28 19:31:05 +0300

    fix run script

--- scripts/run.py
+++ scripts/run.py
@@ -1,18 +1,18 @@
 import os
 from laundry_booker.laundry_booker import Booker 
+from laundry_booker.user_handler import User
 
 # url for the site to book on
 uri = "https://ilmarinen.visiontech.fi/Default.aspx"
 # log and password for site
 login = os.environ['login']
 password = os.environ['password']
-credentials = (login,password)
 # target time for booking in string format (title to find on a web page)
-target_time = '20:00-21:00 (Vapaa)'
-# frequency of checking in hours
-check_time_delta = 1
+target_time = '20:00-21:00'
+
+user = User(uri, login, password, target_time)
 
 if __name__ == "__main__":
-    sniffer = Booker(uri, credentials, target_time, headless=False)
+    sniffer = Booker(user, headless=False)
     sniffer.check()
 
